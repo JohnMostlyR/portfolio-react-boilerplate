ea478246967aff3450f9faa4bed10d7755e56b0a
Johan Meester <walter.doodah@gmail.com>
2017-12-04 13:59:20 +0100

    Adding test to SkillsPage

--- app/containers/SkillsPage/tests/index.test.js
+++ app/containers/SkillsPage/tests/index.test.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import { IntlProvider } from 'react-intl';
+import createHistory from 'history/createMemoryHistory';
 
 import { SkillsPage } from '../index';
+import configureStore from '../../../configureStore';
 
 describe('<SkillsPage />', () => {
   it('should render and match the snapshot', () => {
     const getContent = jest.fn();
     const wrapper = shallow(<SkillsPage getContent={getContent} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call getContent() to load external data', () => {
+    const history = createHistory();
+    const store = configureStore({}, history);
+    const getContent = jest.fn();
+    mount(
+      <Provider store={store}>
+        <IntlProvider locale={'en'}>
+          <SkillsPage getContent={getContent} />
+        </IntlProvider>
+      </Provider>
+    );
+    expect(getContent).toBeCalled();
   });
 });
