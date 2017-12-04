626ae5d1bedef18a721ee7344ffa3ef48e99c0d9
Johan Meester <walter.doodah@gmail.com>
2017-12-04 16:40:26 +0100

    Added extra test to tools.test.js

--- app/styles/tests/tools.test.js
+++ app/styles/tests/tools.test.js
@@ -4,9 +4,15 @@ describe('tools', () => {
   const svg = '"%#}{><';
 
   describe('svgToURL', () => {
-    it('should return a correctly encoded URL string', () => {
+    it('should return a correctly encoded URL string for the passed SVG string', () => {
       const expected = '"data:image/svg+xml;charset=utf-8,\'%25%23%7D%7B%3E%3C"';
       const result = svgToURL(svg);
+      expect(result).toBe(expected);
+    });
+
+    it('should pass through when the passed SVG string is already correctly encoded', () => {
+      const expected = '"data:image/svg+xml;charset=utf-8,svg"';
+      const result = svgToURL('svg');
       expect(result).toBe(expected);
     });
   });
