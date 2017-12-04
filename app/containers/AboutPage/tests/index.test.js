15c0e95de62c9c70b286ac09248e71459aac1a13
Johan Meester <walter.doodah@gmail.com>
2017-12-04 14:00:57 +0100

    Adding test to AboutPage

--- app/containers/AboutPage/tests/index.test.js
+++ app/containers/AboutPage/tests/index.test.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import { IntlProvider } from 'react-intl';
+import createHistory from 'history/createMemoryHistory';
 
 import { AboutPage } from '../index';
+import configureStore from '../../../configureStore';
 
 describe('<AboutPage />', () => {
   it('should render and match the snapshot', () => {
     const getContent = jest.fn();
     const wrapper = shallow(<AboutPage getContent={getContent} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call getContent() to load external data', () => {
+    const history = createHistory();
+    const store = configureStore({}, history);
+    const getContent = jest.fn();
+    mount(
+      <Provider store={store}>
+        <IntlProvider locale={'en'}>
+          <AboutPage getContent={getContent} />
+        </IntlProvider>
+      </Provider>
+    );
+    expect(getContent).toBeCalled();
   });
 });
