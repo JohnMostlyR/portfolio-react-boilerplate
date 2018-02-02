60489eb203414ac5036a07d027ce3d72173dc608
Johan Meester <walter.doodah@gmail.com>
2018-02-02 01:01:22 +0100

    Adding FontFaceObserver

--- app/app.js
+++ app/app.js
@@ -13,6 +13,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
+import FontFaceObserver from 'fontfaceobserver';
 import createHistory from 'history/createBrowserHistory';
 import 'sanitize.css/sanitize.css';
 
@@ -23,7 +24,7 @@ import App from 'containers/App';
 import LanguageProvider from 'containers/LanguageProvider';
 
 // Load the favicon, the manifest.json file and the .htaccess file
-/* eslint-disable import/no-unresolved, import/extensions */
+/* eslint-disable import/no-webpack-loader-syntax */
 import '!file-loader?name=[name].[ext]!./images/favicon.ico';
 import '!file-loader?name=[name].[ext]!./images/favicon-16x16.png';
 import '!file-loader?name=[name].[ext]!./images/favicon-32x32.png';
@@ -35,9 +36,8 @@ import '!file-loader?name=[name].[ext]!./images/mstile-150x150.png';
 import '!file-loader?name=[name].[ext]!./images/mstile-310x150.png';
 import '!file-loader?name=[name].[ext]!./images/mstile-310x310.png';
 import '!file-loader?name=[name].[ext]!./manifest.json';
-import 'file-loader?name=[name].[ext]!./.htaccess';
-import 'file-loader?name=[name].[ext]!./BingSiteAuth.xml';
-/* eslint-enable import/no-unresolved, import/extensions */
+import 'file-loader?name=[name].[ext]!./.htaccess'; // eslint-disable-line import/extensions
+/* eslint-enable import/no-webpack-loader-syntax */
 
 import configureStore from './configureStore';
 
@@ -46,6 +46,17 @@ import { translationMessages } from './i18n';
 
 // Import CSS reset and Global Styles
 import './global-styles';
+
+// Observe loading of Open Sans (to remove open sans, remove the <link> tag in
+// the index.html file and this observer)
+const openSansObserver = new FontFaceObserver('Open Sans', {});
+
+// When Open Sans is loaded, add a font-family using Open Sans to the body
+openSansObserver.load().then(() => {
+  document.body.classList.add('fontLoaded');
+}, () => {
+  document.body.classList.remove('fontLoaded');
+});
 
 // Create redux store with history
 const initialState = {};
