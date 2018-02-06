ecb386724e13348f35e790121b480e18076b7c8e
Johan Meester <walter.doodah@gmail.com>
2018-02-06 01:39:10 +0100

    Adding some shadow to <PageHeader>

--- app/components/PageHeader/StyledHeader.js
+++ app/components/PageHeader/StyledHeader.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const StyledHeader = styled.header`
   float: left;
+  filter: drop-shadow(0 0 10px grey);
   padding-left: 1em;
 `;
 
--- app/components/PageHeader/tests/StyledHeader.test.js
+++ app/components/PageHeader/tests/StyledHeader.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import 'jest-styled-components';
 
 import StyledHeader from '../StyledHeader';
 
 describe('<StyledHeader />', () => {
   it('Should render and match the snapshot', () => {
-    const tree = renderer.create(<StyledHeader />).toJSON();
-    expect(tree).toMatchSnapshot();
+    const wrapper = shallow(<StyledHeader />);
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
