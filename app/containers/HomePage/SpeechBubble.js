d6db82dd0337c742e35b2ae8dd0b9c9d28c33b6c
Johan Meester <walter.doodah@gmail.com>
2018-02-06 13:12:30 +0100

    <HomePage> Adding some shadow to <SpeechBubble>.

--- app/containers/HomePage/SpeechBubble.js
+++ app/containers/HomePage/SpeechBubble.js
@@ -5,7 +5,9 @@ const SpeechBubble = styled.div`
   align-items: center;
   display: flex;
   height: 100%;
+  filter: drop-shadow(0 0 10px grey);
   justify-content: center;
+  padding: 10px 10px 10px 0; /* Keep room for shadow */
 
   @media (min-width: 600px) {
     padding-right: ${rem('114px')}; /* Keep room for site navigation */
--- app/containers/HomePage/tests/SpeechBubble.test.js
+++ app/containers/HomePage/tests/SpeechBubble.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import 'jest-styled-components';
 
 import SpeechBubble from '../SpeechBubble';
 
 describe('<SpeechBubble />', () => {
   it('should render and match the snapshot', () => {
     const renderedComponent = shallow(<SpeechBubble />);
-    expect(renderedComponent).toMatchSnapshot();
+    expect(toJson(renderedComponent)).toMatchSnapshot();
   });
 });
