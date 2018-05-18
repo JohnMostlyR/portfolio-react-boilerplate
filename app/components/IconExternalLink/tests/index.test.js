5bf5478cbed1a9b6505d1839112b4b8563ac625c
Johan Meester <walter.doodah@gmail.com>
2018-05-18 13:08:11 +0200

    [New] Adding the IconExternalLink component.

---
+++ app/components/IconExternalLink/tests/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import IconExternalLink from '../index';
+
+describe('<IconExternalLink />', () => {
+  it('should render and match the snapshot', () => {
+    const renderedComponent = shallow(<IconExternalLink />);
+    expect(toJson(renderedComponent)).toMatchSnapshot();
+  });
+
+  it('should adopt props', () => {
+    const renderedComponent = shallow(<IconExternalLink height="99px" width="99px" color="green" />);
+    expect(toJson(renderedComponent)).toMatchSnapshot();
+  });
+});
