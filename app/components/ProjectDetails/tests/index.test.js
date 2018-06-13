43eba6e931784720f779741f1ddd41e75afa7171
Johan Meester <walter.doodah@gmail.com>
2018-06-13 11:33:09 +0200

    [Improvement] Adding scroll restoration so that the project details starts at the top of the screen.

--- app/components/ProjectDetails/tests/index.test.js
+++ app/components/ProjectDetails/tests/index.test.js
@@ -9,17 +9,60 @@ describe('<ProjectDetails />', () => {
     article: '# Article for test case\n\n[add attributes to link](https://the.world/)\n',
     description: 'Description for test case',
     images: [
-      { file: { fileName: 'test-s.png', url: '/test-s.png' }, title: 'Test image' },
-      { file: { fileName: 'test-m.png', url: '/test-m.png' }, title: 'Test image' },
-      { file: { fileName: 'test-l.png', url: '/test-l.png' }, title: 'Test image' },
-      { file: { fileName: 'test-xl.png', url: '/test-xl.png' }, title: 'Test image' },
+      {
+        file: {
+          details: {
+            image: {
+              height: 10,
+              width: 20,
+            },
+          },
+          fileName: 'test-s.png',
+          url: '/test-s.png',
+        },
+        title: 'Test image',
+      },
+      {
+        file: {
+          details: {
+            image: {
+              height: 20,
+              width: 30,
+            },
+          },
+          fileName: 'test-m.png',
+          url: '/test-m.png',
+        },
+        title: 'Test image',
+      },
+      {
+        file: {
+          details: {
+            image: {
+              height: 30,
+              width: 40,
+            },
+          },
+          fileName: 'test-l.png',
+          url: '/test-l.png',
+        },
+        title: 'Test image',
+      },
+      {
+        file: {
+          details: {
+            image: {
+              height: 50,
+              width: 60,
+            },
+          },
+          fileName: 'test-xl.png',
+          url: '/test-xl.png',
+        },
+        title: 'Test image',
+      },
     ],
     title: 'Title for test case',
-  });
-
-  it('Should render without any given properties', () => {
-    const wrapper = shallow(<ProjectDetails />);
-    expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   it('Should adopt the "project" property', () => {
@@ -27,8 +70,8 @@ describe('<ProjectDetails />', () => {
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
-  it('Should handle case where "images" is not an Array', () => {
-    const projectWithImagesNotBeingAnArray = Object.assign({}, project, { images: '' });
+  it('Should handle case where "images" is not provided', () => {
+    const projectWithImagesNotBeingAnArray = Object.assign({}, project, { images: [] });
     const wrapper = shallow(<ProjectDetails project={projectWithImagesNotBeingAnArray} />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
