310942ac179568b919b8d5488cce366ae8cec1a1
Johan Meester <walter.doodah@gmail.com>
2018-07-05 08:40:28 +0200

    [Update] In case language is not set it's most likely an indexer and in that case we want to push 'nl'

--- app/containers/LanguageProvider/reducer.js
+++ app/containers/LanguageProvider/reducer.js
@@ -10,13 +10,19 @@ import { CHANGE_LANGUAGE } from './constants';
 import { config } from '../../config'; // eslint-disable-line
 
 const { appLocales, defaultLocale } = config;
-const {
-  navigator: { language },
-} = window;
-let locale = language.substr(0, 2).toLowerCase();
+const { navigator: { language = '' } = {} } = window;
+let locale = defaultLocale;
 
-if (!appLocales.indexOf(locale)) {
-  locale = defaultLocale;
+/**
+ * In case language is not set it's most likely an indexer
+ * and in that case we want to push 'nl'
+ */
+if (language) {
+  if (appLocales.indexOf(language) !== -1) {
+    locale = language.substr(0, 2).toLowerCase();
+  }
+} else {
+  locale = 'nl';
 }
 
 export const initialState = fromJS({
